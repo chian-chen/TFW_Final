80ff016632159367dd88d5f593080169cad87450
chian-chen <64936325+chian-chen@users.noreply.github.com>
2023-01-05 19:52:45 +0800

    add some ref, separate function

---
+++ Ambiguity.m
@@ -0,0 +1,40 @@
+function y = Ambiguity(x, t, f)
+
+    P = length(t);  F = length(f);  % T,F
+    dt = t(2) - t(1);   % dt
+    df = f(2) - f(1);   % df
+    
+    mo = round(f/df);  % mo
+%     no = round(t/dt);  % no
+
+    N = round(1/dt/df);
+    y = zeros(P, F); 
+
+    index = find(x);
+    n1 = index(1);
+    n2 = index(end);
+    
+    
+    for p = 1:P
+        
+        pp = p;
+        if(pp > 1001)
+            pp = pp - 1001;
+        else
+            pp = 1001 - pp;
+        end
+
+        Q = n2 - n1 - 2 * pp;
+        c1 = zeros(1, N);
+        
+        for n = 0 : Q
+            c1(n + 1) = x(n + n1 + 2 * pp) * conj(x(n + n1));
+        end
+
+        C = fft(c1);
+
+        for m = 1:F
+            y(p, m) = C(mod(mo(m), N)+1)*exp(-1i * 2 * pi * Q * mo(m)/N)*dt;
+        end
+    end
+end
--- CohenClass.m
+++ CohenClass.m
@@ -30,17 +30,19 @@ ylabel('Frequency (Hz)','Fontsize',12);
 xlabel('Time (Sec)','Fontsize',12);
 title('STFT of x(t)','Fontsize',12);
 
-
+% ===============
 % low pass filter
+
 
 [m, n] = size(y);
 Filter = zeros(m, n);
 centerf = m / 2;
 centert = n / 2;
+alpha = 1;
 
 for i = 1:m
     for j = 1:n
-        Filter(i, j) = sinc((i - centerf)*df * (j - centert)*dt)*exp(-2 * pi * ((j - centert)*dt)^2); 
+        Filter(i, j) = sinc((i - centerf)*df * (j - centert)*dt)*exp(-2 * pi * alpha * ((j - centert)*dt)^2); 
     end
 end
 
@@ -60,45 +62,3 @@ y = y .* Filter;
 % ================
 
 
-
-
-function y = Ambiguity(x, t, f)
-
-    P = length(t);  F = length(f);  % T,F
-    dt = t(2) - t(1);   % dt
-    df = f(2) - f(1);   % df
-    
-    mo = round(f/df);  % mo
-%     no = round(t/dt);  % no
-
-    N = round(1/dt/df);
-    y = zeros(P, F); 
-
-    index = find(x);
-    n1 = index(1);
-    n2 = index(end);
-    
-    
-    for p = 1:P
-        
-        pp = p;
-        if(pp > 1001)
-            pp = pp - 1001;
-        else
-            pp = 1001 - pp;
-        end
-
-        Q = n2 - n1 - 2 * pp;
-        c1 = zeros(1, N);
-        
-        for n = 0 : Q
-            c1(n + 1) = x(n + n1 + 2 * pp) * conj(x(n + n1));
-        end
-
-        C = fft(c1);
-
-        for m = 1:F
-            y(p, m) = C(mod(mo(m), N)+1)*exp(-1i * 2 * pi * Q * mo(m)/N)*dt;
-        end
-    end
-end
